45da552373e1a924817f7ecef013cf5fb1e5dafa
Blake Appleby <alexappleby70@gmail.com>
2025-04-22 22:43:14 -0400

    comitting changes for tonight

--- server.py
+++ server.py
@@ -3,6 +3,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+quiz_qs = [
+    {
+        "mains": 2,
+        "complements": 1,
+        "choices": [
+            {"image": "/static/blue_shirt.jpg", "color": "blue"},
+            {"image": "/static/black_leather_jacket.webp", "color": "black"},
+            {"image": "/static/carhartt_jacket.webp", "color": "brown"},
+            {"image": "/static/jorts.jpg", "color": "blue"},
+            {"image": "/static/white_pants.webp", "color": "white"},
+            {"image": "/static/yellow_shirt.jpg", "color": "yellow"},
+        ],
+    },
+]
+
 
 @app.route("/")
 def home():
@@ -11,7 +26,7 @@ def home():
 
 @app.route("/quiz")
 def quiz():
-    return render_template("quiz.html")
+    return render_template("quiz.html", question=quiz_qs[0])
 
 
 if __name__ == "__main__":
--- static/quiz.js
+++ static/quiz.js
@@ -1,7 +1,43 @@
-// some JS goes here i guess
-
-
 $(function () {
+    const mains = [];
+    const complements = [];
+
+    function storeState() {
+        mains.length = 0;
+        complements.length = 0;
+
+        $('#mains .quizbox').each(function () {
+            mains.push($(this).data('color'));
+        });
+
+        $('#complements .quizbox').each(function () {
+            complements.push($(this).data('color'));
+        });
+
+        console.log('Mains:', mains);
+        console.log('Complements:', complements);
+
+        const mainDisplays = $('.main-display');
+        const complementDisplays = $('.complement-display');
+
+        mainDisplays.each(function (i) {
+            const el = $(this);
+            const baseClasses = el.attr('class').split(' ').filter(c => c !== undefined && !mains.includes(c));
+            el.attr('class', baseClasses.join(' '));
+            if (i < mains.length) {
+                el.addClass(mains[i]);
+            }
+        });
+
+        complementDisplays.each(function (i) {
+            const el = $(this);
+            const baseClasses = el.attr('class').split(' ').filter(c => c !== undefined && !complements.includes(c));
+            el.attr('class', baseClasses.join(' '));
+            if (i < complements.length) {
+                el.addClass(complements[i]);
+            }
+        });
+    }
     $(".quizbox").draggable({
       revert: "invalid",
       helper: "original",
@@ -12,8 +48,9 @@ $(function () {
       drop: function (event, ui) {
         ui.draggable
           .detach()
-          .css({ top: "", left: "", position: "" })  // removes absolute position
+          .css({ top: 0, left: 0 })
           .appendTo(this);
+        storeState();
       }
     });
 
@@ -21,8 +58,19 @@ $(function () {
       drop: function (event, ui) {
         ui.draggable
           .detach()
-          .css({ top: "", left: "", position: "" })  // removes absolute position
+          .css({ top: 0, left: 0 })
           .appendTo(this);
+        storeState();
+      }
+    });
+
+    $("#closet").droppable({
+      drop: function (event, ui) {
+        ui.draggable
+          .detach()
+          .css({ top: 0, left: 0 })
+          .appendTo(this);
+        storeState();
       }
     });
 });
